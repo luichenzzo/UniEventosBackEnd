20fe4a5e9525d7db5078f9727a96d573ac529b9b
Lamsu1505 <andrese.perezm@uqvirtual.edu.co>
2024-10-07 11:26:38 -0500

    DTO de cliente implementado
    d

---
+++ src/main/java/co/edu/uniquindio/unieventos/dto/client/ClientResponseDTO.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.unieventos.dto.client;
+
+public record ClientResponseDTO(
+        String id,
+        String firstName,
+        String lastName,
+        String address,
+        String phoneNumber,
+        String email
+) {
+}
